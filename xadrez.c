4f07826fbb979644e3129c32d1557c6bce29e0a9
Hb-Progr <hiago_mt@live.com>
2025-11-10 20:34:04 +0000

    Desafio xadrez finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 
-int main() {
-
-    int dir, passos;
-
-    printf("===Torre===\n");
-    printf("Escolha a direção da Torrre:\n");
-    printf("1) Direta 2) Esquerda 3) Cima 4) Baixo\n ");
-    printf("Opção: ");
-    scanf("%d", &dir);
-
-    printf("Quantos passos? ");
-    scanf("%d", &passos);
-
-    printf("\nMovimento da Torre:\n");
-    for(int i = 0; i < passos; i++) {
+void moverTorre(int passos, int dir) {
+    if(passos <=0) return;
         switch (dir) {
             case 1: printf("Direita\n");
             break;
@@ -25,59 +12,14 @@ int main() {
             case 4: printf("Baixo\n");
             break;
 
-            defaut:printf("Direção invalida\n"); i = passos; break;
-        }
+            defaut:printf("Direção invalida\n"); return;
+        }moverTorre(passos - 1, dir);
     }
 
-    printf ("\n");
-
-    printf("=== Bispo ===\n");
-    printf("Escolha a diagonal do Bispo:\n");
-    printf("1) Cima Direita 2) Cima Esquerda 3) Baixo Direita 4) Baixo Esquerda\n");
-    printf("Opção: ");
-    scanf("%d", &dir);
-
-    printf("Quantos passos? ");
-    scanf("%d", &passos);
-
-    printf("\nMovimento do Bispo:\n");
-    int j = 0;
-    if (passos <= 0) {
-        printf("Nada a mover.\n");
-    } else {
-        do {
-            switch (dir){
-                case 1: printf("Cima Direita\n");
-                break;
-                case 2: printf("Cima Esquerda\n");
-                break;
-                case 3: printf("Baixo Direita\n");
-                break;
-                case 4: printf("Baixo Esquerda\n");
-                break;
-                default:printf("Direção invalida\n");
-                j = passos; break;
-
-            }
-            j++;
-        } while (j < passos);
-        printf("\n");
-    }
-     printf("=== Rainha ===\n");
-     printf("Escolha a direção da Rainha: \n");
-     printf("1) Direita 2) Esquerda 3) Cima 4) Baixo\n");
-     printf("5) Cima Direita 6) Cima Esquerda 7)Baixo Direita 8)Baixo Esquerda\n");
-     printf("Opção: ");
-     scanf("%d", &dir);
-
-     printf("Quantos passos? ");
-     scanf("%d", &passos);
-
-     printf("\nMovimento da Rainha:\n");
-     int k = 0;
-     while (k < passos) {
-        switch (dir) {
-        case 1: printf ("Direita\n");
+void moverRainha(int passos, int dir){
+    if(passos <= 0) return;
+    switch (dir){
+          case 1: printf ("Direita\n");
         break;
         case 2: printf("Esquerda\n");
         break;
@@ -93,11 +35,112 @@ int main() {
         break;
         case 8: printf("Baixo Esquerda\n");
         break;
-        default: printf("Direção invalida");
-        k = passos; break;
-        }
-        k++; 
+        default: printf("Direção invalida"); return;      
+} moverRainha(passos - 1,dir);
+}
+
+
+void moverBispo(int passos, int dir) {
+    if(passos <= 0) return;
+    switch (dir){
+                case 1: printf("Cima Direita\n");
+                break;
+                case 2: printf("Cima Esquerda\n");
+                break;
+                case 3: printf("Baixo Direita\n");
+                break;
+                case 4: printf("Baixo Esquerda\n");
+                break;
+                default:printf("Direção invalida\n");return;
+    } moverBispo(passos - 1, dir);
+}
+void moverCavalo(int dir, int etapa) {
+    if(etapa == 0) return;
+    switch (dir) 
+
+    case 1:
+      if (etapa == 2)
+       printf("Baixo\n");
+       else if (etapa == 1){
+        printf("Baixo\nEsquerda\n");
+        moverCavalo(dir, etapa - 1);
+      
+        break;
+           
+    case 2: 
+        if(etapa == 2)
+        printf("Baixo\n");
+        else if (etapa == 1)
+        printf("Baixo\nDireita\n");
+        moverCavalo(dir,etapa - 1);
+        break;
+    
+     case 3: 
+        if (etapa ==2)
+         printf("Cima\n");
+         else if(etapa == 1)
+         printf("Cima\nDireia\n");
+         moverCavalo(dir, etapa - 1);
+         break;
+     
+     case 4: 
+        if(etapa == 2)
+        printf("Cima\n");
+        else if(etapa ==1)
+        printf("Cima\nDireita\n");
+        moverCavalo(dir, etapa -1);
+        break;
+     
+     default:
+     printf("Direção invalida");
+        return;
+    
     }
+    
+}
+
+int main() {
+ int dir; 
+ int passos;
+
+    printf("===Torre===\n");
+    printf("Escolha a direção da Torrre:\n");
+    printf("1) Direta 2) Esquerda 3) Cima 4) Baixo\n ");
+    printf("Opção: ");
+    scanf("%d", &dir);
+
+    printf("Quantos passos? ");
+    scanf("%d", &passos);
+
+    printf("Movimento da Torre:\n");
+    moverTorre(passos,dir);
+    printf("\n");
+
+    printf("=== Bispo ===\n");
+    printf("Escolha a diagonal do Bispo:\n");
+    printf("1) Cima Direita 2) Cima Esquerda 3) Baixo Direita 4) Baixo Esquerda\n");
+    printf("Opção: ");
+    scanf("%d", &dir);
+
+    printf("Quantos passos? ");
+    scanf("%d", &passos);
+
+    printf("\nMovimento do Bispo:\n");
+    moverBispo(passos,dir);
+    printf("\n");
+    
+     printf("=== Rainha ===\n");
+     printf("Escolha a direção da Rainha: \n");
+     printf("1) Direita 2) Esquerda 3) Cima 4) Baixo\n");
+     printf("5) Cima Direita 6) Cima Esquerda 7)Baixo Direita 8)Baixo Esquerda\n");
+     printf("Opção: ");
+     scanf("%d", &dir);
+
+     printf("Quantos passos? ");
+     scanf("%d", &passos);
+
+     printf("\nMovimento da Rainha:\n");
+     moverRainha(passos,dir);
      printf("\n");
 
      printf("=== Cavalo ===\n");
@@ -110,61 +153,8 @@ int main() {
      scanf("%d", &dir);
 
      printf("\nMovimentação do Cavalo:\n");
-
-     switch(dir) {
-        case 1:
-        for(int i = 0; i < 2; i++) {
-            printf("Baixo\n");
-        }
-        {
-            int j = 0;
-            while(j < 1) {
-                printf("Esquerda\n");
-                j++;
-            }
-          }
-          break;
-          case 2:
-          for(int i = 0; i < 2; i++) {
-            printf("Baixo\n");
-          }
-          {
-             int j = 0;
-             while(j < 1) {
-                printf("Direita\n");
-                j++;
-             }
-          }
-          break;
-          case 3:
-          for(int i = 0; i < 2; i++) {
-            printf("Cima\n");
-          }
-          {
-            int j = 0;
-            while(j < 1) {
-                printf("Esquerda\n");
-                j++;
-            }
-          }
-          break;
-          case 4:
-          for(int i = 0; i < 2; i++) {
-            printf("Cima\n");
-          }
-          {
-            int j = 0;
-            while(j < 1){
-                printf("Direta\n");
-                j++;
-            }
-          }
-          break;
-          default:
-          printf("Movimento inválido!\n");
-          
-          }
-          printf("\n");
+     moverCavalo(dir,2);
+     printf("\n");
 
     return 0;
 }
